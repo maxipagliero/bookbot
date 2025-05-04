959b2e3d6868b6c4b67654513e266325a673590b
maxipagliero <maxipagliero@gmail.com>
2025-05-04 18:41:04 +0200

    C: character counter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import count_words    
+from stats import count_words, character_count
 
 def main():
     count_words("./books/frankenstein.txt")
+    character_count("./books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -3,10 +3,21 @@ def count_words(path_to_file):
         file_contents = f.read()
     
     words = file_contents.split()
-    
-    total_words = 0
 
-    for word in words:
-        total_words += 1
+    total_words = len(words)
 
     print(f"{total_words} words found in the document")
+
+def character_count(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    
+    letter_dict = {}
+
+    for letter in file_contents.lower():
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    
+    print(letter_dict)
