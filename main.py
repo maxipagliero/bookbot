ef88076b88a200499adb0bda49a86d209d2da590
maxipagliero <maxipagliero@gmail.com>
2025-05-05 17:25:01 +0200

    D: new function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import book_stats
 
 def main():
-    book_stats("./books/frankenstein.txt")
+    book_stats("books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -2,18 +2,29 @@ def book_stats(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
 
+
     letter_dict = {}
 
     for letter in file_contents.lower():
-        if letter in letter_dict:
-            letter_dict[letter] += 1
-        else:
-            letter_dict[letter] = 1
+        if letter.isalpha():
+            if letter in letter_dict:
+                letter_dict[letter] += 1
+            else:
+                letter_dict[letter] = 1
     
+    items = list(letter_dict.items())
+
+    items.sort(key=lambda item: item[1], reverse=True)
+
     words = file_contents.split()
 
     total_words = len(words)
 
-    print(f"{total_words} words found in the document")
-
-    print(letter_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for item in items:
+        print(f"{item[0]}: {item[1]}")
+    print("============= END ===============")
